5eca029c17d8b7bdb09671b20bf6ff37807e2f57
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-01-02 20:35:23 +1100

    Create initial Java 6 compatible project template
    - Should be compatible with Gradle 6.8 when it comes out
    - Compatible with Java 6 through use of Gradle toolchains (Gradle doesn't currently work with older versions of Java)
    - GitHub actions is used to build and test the project, as well as to minimise the size of the built .jar files

---
+++ src/main/java/com/github/NeRdTheNed/example/Example.java
@@ -0,0 +1,10 @@
+package com.github.NeRdTheNed.example;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("This is an template / example project!");
+        // TODO the rest of the project!
+    }
+
+}
